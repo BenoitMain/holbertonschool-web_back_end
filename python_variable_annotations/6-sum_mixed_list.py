fe22ea1e6235e449e5ebac8317947749fd977e42
BenoitMain <benoitmaingon@gmail.com>
2025-07-28 14:35:50 +0200

    Fix formatting by adding a blank line before the sum_mixed_list function definition

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -9,6 +9,7 @@ Functions:
         Returns the sum of a list containing integers and floats.
 """
 
+
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     Returns the sum of a list containing integers and floats.
