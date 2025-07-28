4606313dfdfa1e642e5655a574e9e6c05c912019
BenoitMain <benoitmaingon@gmail.com>
2025-07-28 15:13:39 +0200

    Fix import formatting in sum_list.py by adjusting whitespace

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-from typing import  List
 """
 Module for summing a list of floating point numbers.
 
 """
+
+
+from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
