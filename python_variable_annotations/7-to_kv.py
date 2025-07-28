767e622a38ef07714d273eebbbd3cbc9328a7199
BenoitMain <benoitmaingon@gmail.com>
2025-07-28 15:15:52 +0200

    Reorder import statements in 7-to_kv.py for consistency

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-from typing import Union, Tuple
-
 """
 Module for creating a tuple from a string and the square of an int or float.
 
@@ -8,6 +6,9 @@ Functions:
     to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
         Returns a tuple with the string and the square of the value as a float.
 """
+
+
+from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
