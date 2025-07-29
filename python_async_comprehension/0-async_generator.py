5d528a69425301506932f4c52c9868b6658a1be9
BenoitMain <benoitmaingon@gmail.com>
2025-07-29 16:37:28 +0200

    Refactor async_generator to use Generator type hint instead of AsyncGenerator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,10 +5,10 @@ Asynchronously yields 10 random float numbers between 0 and 10.
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
+async def async_generator() -> Generator[float, None]:
     """
     This coroutine waits 1 second between each yield and produces a
     random floating-point number in the range [0, 10) using the random module.
