ff07027e98014c25ad0471ddf46f3f04db525dc3
BenoitMain <benoitmaingon@gmail.com>
2025-07-29 15:59:59 +0200

    Add type hint for async_generator function to specify return type

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,9 +4,10 @@ Asynchronously yields 10 random float numbers between 0 and 10.
 """
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     This coroutine waits 1 second between each yield and produces a
     random floating-point number in the range [0, 10) using the random module.
