b00fcad6ef58b1faf70854da5b4f57c4dbbcef36
BenoitMain <benoitmaingon@gmail.com>
2025-07-29 12:05:46 +0200

    Fix formatting by adding a blank line before the task_wait_random function

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -8,6 +8,7 @@ import importlib
 
 wait_random = importlib.import_module("0-basic_async_syntax").wait_random
 
+
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """
     Create and return an asyncio.Task that wraps wait_random.
