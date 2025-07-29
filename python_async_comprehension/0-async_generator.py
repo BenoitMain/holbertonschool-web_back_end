630b744b62f4028c35ebeadc8f29e6918facc822
BenoitMain <benoitmaingon@gmail.com>
2025-07-29 16:36:32 +0200

    Refactor async_generator to include additional type hint and simplify yield statement

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,7 +8,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> AsyncGenerator[float, None, None]:
     """
     This coroutine waits 1 second between each yield and produces a
     random floating-point number in the range [0, 10) using the random module.
@@ -18,4 +18,5 @@ async def async_generator() -> AsyncGenerator[float, None]:
     """
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        float_number = random.uniform(0, 10)
+        yield float_number
