4198538a956220d5f44c06dcb41b82dbbe9b9746
BenoitMain <benoitmaingon@gmail.com>
2025-08-18 14:49:47 +0200

    refactor: simplify return statement in concatArrays function

--- ES6_basic/5-spread-operator.js
+++ ES6_basic/5-spread-operator.js
@@ -1,3 +1,4 @@
 export default function concatArrays(array1, array2, string) {
-    return [...array1, ...array2, ...string];
+    const concatenate = [...array1, ...array2, ...string]
+    return concatenate
 }
