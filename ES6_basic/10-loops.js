a322b135295f179f849491aa521235747194ccdc
BenoitMain <benoitmaingon@gmail.com>
2025-08-19 08:59:38 +0200

    fix: correct indentation in appendToEachArrayValue function

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-	const tableau = [];
-	for (const value of array) {
-		tableau.push(appendString + value);
-	}
+  const tableau = [];
+  for (const value of array) {
+    tableau.push(appendString + value);
+  }
 
-	return tableau;
+  return tableau;
 }
