efece75af8526e4000f6e27eccc1de746396d0ad
BenoitMain <benoitmaingon@gmail.com>
2025-08-19 08:58:35 +0200

    fix: correct syntax by removing unnecessary semicolon in appendToEachArrayValue function

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -2,7 +2,7 @@ export default function appendToEachArrayValue(array, appendString) {
 	const tableau = [];
 	for (const value of array) {
 		tableau.push(appendString + value);
-	};
+	}
 
 	return tableau;
 }
