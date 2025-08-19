3adb43f5c617dd2dbf8d7c9829ab8862296573bc
BenoitMain <benoitmaingon@gmail.com>
2025-08-19 08:51:41 +0200

    fix: correct syntax by removing unnecessary semicolon in appendToEachArrayValue function

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -2,6 +2,6 @@ export default function appendToEachArrayValue(array, appendString) {
   array = [];
   for (const value of array) {
     array.push(appendString + value);
-  }
+  };
   return array;
 }
