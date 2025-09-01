71a32978a216b2a71fadf7bc061e5690fc2a00b6
BenoitMain <benoitmaingon@gmail.com>
2025-09-01 13:54:21 +0200

    style: format code for better readability in stdin input handling

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,8 +2,11 @@ console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
-  if (name !== null) process.stdout.write(`Your name is: ${name}`);
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
+
 process.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
