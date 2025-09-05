9733b7364a8a0f2e3e2646d64be30730dcbd5e48
BenoitMain <benoitmaingon@gmail.com>
2025-09-05 23:18:09 +0200

    fix: refactor getAllStudents method for improved readability and consistency

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,39 +1,41 @@
-import readDatabase from "../utils.js";
+import readDatabase from '../utils';
 
 export default class StudentsController {
-    static getAllStudents(req, res) {
-        let responseText = 'This is the list of our students';
-        const path = process.argv[2];
-        readDatabase(path)
-        .then((data) => {
-            const sorteFields = Object.keys(data).sort();
+  static getAllStudents(req, res) {
+    res.status(200).setHeader('Content-Type', 'text/plain');
+    const databasePath = process.argv[2];
+    readDatabase(databasePath)
+      .then((data) => {
+        let responseText = 'This is the list of our students\n';
+        const fields = Object.keys(data).sort((a, b) => a.localeCompare(b, 'en', { sensitivity: 'base' }));
+        for (const f of fields) {
+          const students = data[f];
+          responseText += `Number of students in ${f}: ${students.length}. List: ${students.join(', ')}\n`;
+        }
+        res.send(responseText);
+      })
+      .catch(() => {
+        res.status(500).send('Cannot load the database');
+      });
+  }
 
-            for (const field of sorteFields) {
-                const students = data[field];
-                const count = students.length
-                const studentList = students.join(', ');
-                responseText += `\nNumber of students in ${field}: ${count}. List: ${studentList}`;
-            }
-            res.status(200).send(responseText);
-        })
-        .catch(() => {
-            res.status(500).send(`Cannot load the database`)
-        });
+  static getAllStudentsByMajor(req, res) {
+    res.status(200).setHeader('Content-Type', 'text/plain');
+    const { major } = req.params;
+
+    if (major !== 'CS' && major !== 'SWE') {
+      return res.status(500).send('Major parameter must be CS or SWE');
     }
+    const databasePath = process.argv[2];
+    readDatabase(databasePath)
+      .then((data) => {
+        const students = data[major] || [];
+        res.send(`List: ${students.join(', ')}`);
+      })
+      .catch(() => {
+        res.status(500).send('Cannot load the database');
+      });
 
-    static getAllStudentsByMajor(req, res) {
-        const path = process.argv[2];
-        const { major } = req.params;
-        if( req.params.major !== 'CS' &&  req.params.major !== 'SWE'){
-            return res.status(500).send('Major parameter must be CS or SWE');
-        }
-            readDatabase(path)
-            .then((students) => {
-            const studentList = students[major];
-            res.status(200).send(`List: ${studentList.join(', ')}`);
-            })
-        .catch(() => {
-            res.status(500).send('Cannot load the database');
-         });
-        }
+    return undefined;
+  }
 }
